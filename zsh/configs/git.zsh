c36a61aa2e22a4e66140fa29ad76b50044bde8be
jKiler <jakwiatkowski@gmail.com>
2024-04-05 22:53:26 +0200

    feat: update zsh git config

--- zsh/configs/git.zsh
+++ zsh/configs/git.zsh
@@ -6,3 +6,28 @@ function g {
     git status --short --branch
   fi
 }
+
+function convc {
+  local m="$1: "
+
+  local branch=$(git rev-parse --abbrev-ref HEAD)
+  if [[ $branch =~ ([A-Z]+-[0-9]+) ]]; then
+    m+="${BASH_REMATCH[1]} "
+  fi
+
+  m+="$2"
+
+  git commit -m "$m" && git l -1
+}
+
+function gfe {
+  convc "feat" "$1"
+}
+
+function gfi {
+  convc "fix" "$1"
+}
+
+function gre {
+  convc "refactor" "$1"
+}
